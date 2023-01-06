2413d674b6bcc67debba1ecb3b767733e4a4863f
rinkuberde <rinkuberde@gmail.com>
2023-01-06 20:42:34 +0530

    [Rinku]-  Repeat till the Player reaches 100

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -3,30 +3,42 @@
     internal class SnakeLadder
     {
       static void Main(string[] args)
-        {
+        { 
+            //variable
             int position = 0, rollDice;
             //create random number of rolling dice
             Random random = new Random();
+
+            //list to check option 
+            var list = new List<string> { "No Play", "Ladder", "Snake" };
+
+            //user condition
             rollDice = random.Next(1, 7);
             Console.WriteLine("Rolled Dice number is:" + rollDice);
             //print user option 
             Console.WriteLine("player option 1.No Play 2.Ladder 3.Snake");
-            var list = new List<string> { "No Play", "Ladder", "Snake" };
-            //check user option
-            int pos = random.Next(list.Count);
-            //condition based on user option
-            if (list[pos] == "Ladder")
+
+            //random to check user option
+            int index = random.Next(list.Count);
+            Console.WriteLine("Player option is: " + list[index]);
+
+            if (position + rollDice< 100)
             {
-                position += rollDice;
-            }
-            else if (list[pos] == "Snake")
-            {
-                position -= rollDice;
+                if (list[index] == "Ladder") position += rollDice;
+                if (list[index] == "Snake") position -= rollDice;
             }
 
+            if (position < 0)
+            {
+                position = 0;
+            }
+            //print to console
+            Console.WriteLine("\n");
+            
+
             //print result of playerPosition and dice rolled
-            Console.WriteLine("Player option:" + list[pos]);
-            Console.WriteLine("Player updated postion:" + position );
+            Console.WriteLine("Player current position:" + position);
+            Console.WriteLine("Player final postion:" + position );
 
         }      
         
